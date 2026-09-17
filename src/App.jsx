5cee521e323a06a09e0a069ea23c0c9726ce120e
Adarsh Kumar <techadarsh006@gmail.com>
2026-09-17 10:24:23 +0530

    Add Call & Whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Routes, Route } from "react-router-dom";
-
+import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
